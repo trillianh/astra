23fdc78c6e66ebd9b28986af3307788079dd854e
k <trillianh7@gmail.com>
2018-03-28 19:34:43 -0700

    test

--- astra.js
+++ astra.js
@@ -221,7 +221,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                          logger.info(smembers.length);
                          var memberstr = "";
                          for(var i in smembers){
-                             if(bot.channels[trillianAstra].members[smembers[i]]){
+                             if(bot.servers[serverid].channels[trillianAstra].members[smembers[i]]){
                                 memberstr += smembers[i].id+" ";
                              }
                          }
