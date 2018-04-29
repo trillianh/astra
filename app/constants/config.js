9b1211febaa53f8237b4997c6db5cc40c66cc796
mooryong <hyunsikk@gmail.com>
2018-04-29 11:55:20 -0700

    Add officer remove cmd with args

--- app/constants/config.js
+++ app/constants/config.js
@@ -2,3 +2,4 @@ export const BOT_TOKEN = process.env.BOT_TOKEN;
 export const CHANNEL_ID = process.env.CHANNEL_ID;
 export const DB_NAME = process.env.DB_NAME;
 export const MONGO_DB_HOST = process.env.MONGO_DB_HOST;
+export const OFFICER_IDS = process.env.OFFICER_IDS;
