da5c5cdd992b4a78e2969a70e9276a9083913e28
mooryong <38062073+mooryong@users.noreply.github.com>
2018-04-29 12:07:21 -0700

    Update info cmd and add officer remove cmd (#12)
    * Add mode data to info cmd
    
    * Add officer remove cmd with args
    
    * Create new file for officer command

---
+++ app/classes/officer.js
@@ -0,0 +1,56 @@
+import { Character } from '../models/character';
+import { list } from '../commands/list';
+import { add } from '../commands/add';
+import { get } from '../commands/get';
+import { update } from '../commands/update';
+import { reroll } from '../commands/reroll';
+import { help } from '../commands/help';
+import { cc } from '../commands/cc';
+import { remove } from '../commands/officer/remove';
+import { roll } from '../commands/roll';
+import { addpic } from '../commands/addpic';
+import { info } from '../commands/info';
+
+class Officer {
+  static perform(command, discordId, args, replyCallback) {
+    switch(command) {
+      case 'add':
+        add(args, discordId, replyCallback);
+        break;
+      case 'addpic':
+        addpic(args, discordId, replyCallback);
+        break;
+      case 'get':
+        get(args, discordId, replyCallback);
+        break;
+      case 'update':
+        update(args, discordId, replyCallback);
+        break;
+      case 'list':
+        list(args, replyCallback);
+        break;
+      case 'reroll':
+        reroll(args, discordId, replyCallback);
+        break;
+      case 'remove':
+        remove(discordId, args, replyCallback);
+        break;
+      case 'roll':
+        roll(args, replyCallback);
+        break;
+      case 'cc':
+        cc(discordId, replyCallback);
+        break;
+      case 'help':
+        help(args, replyCallback);
+        break;
+      case 'info':
+        info(replyCallback);
+        break;
+      default:
+        replyCallback('Invalid Command try .help');
+    }
+  };
+};
+
+export { Officer };
---
+++ app/commands/officer/remove.js
@@ -0,0 +1,18 @@
+import {
+  Character
+} from '../../models/character';
+
+function remove(discordId, args, callback) {
+  const discordIdToRemove = _removeMember(args) ? args[0] : discordId;
+
+  Character.deleteOne({ discord_id: discordIdToRemove }).then((record) => {
+    console.log(record);
+    callback('Successfully deleted');
+  });
+};
+
+function _removeMember(args) {
+  return (args && args.length > 0 && args[0]);
+};
+
+export { remove };
