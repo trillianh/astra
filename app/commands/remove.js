b889e9410830004c069747b742d2f09a393a64b6
trillianh <35315122+trillianh@users.noreply.github.com>
2020-05-04 16:37:43 -0700

    Update remove.js

--- app/commands/remove.js
+++ app/commands/remove.js
@@ -5,7 +5,7 @@ import {
 function remove(discordId, callback) {
   Character.deleteOne({ discord_id: discordId }).then((record) => {
     console.log(record);
-    callback('Successfully deleted');
+    callback('Successfully deleted your data');
   });
 };
 
