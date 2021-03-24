534c45cae4291564215022ef5edb11deedd6d33c
trillianh <35315122+trillianh@users.noreply.github.com>
2021-03-23 20:02:01 -0700

    Update config.js

--- app/constants/config.js
+++ app/constants/config.js
@@ -3,3 +3,4 @@ export const CHANNEL_ID = process.env.CHANNEL_ID;
 export const DB_NAME = process.env.DB_NAME;
 export const MONGO_DB_HOST = process.env.MONGO_DB_HOST;
 export const OFFICER_IDS = process.env.OFFICER_IDS;
+export const ALT_CHANNEL = process.env.ALT_CHANNEL;
